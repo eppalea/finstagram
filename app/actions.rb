5883a5c8a70a679cba227f5ee101a1db02c42e16
eppalea <eppalea@gmail.com>
2020-12-15 04:27:57 +0000

    add f_post, comment, like functionality

--- app/actions.rb
+++ app/actions.rb
@@ -59,3 +59,67 @@ get '/logout' do
     session[:user_id] = nil
     redirect to('/')
 end
+
+# create new post
+post '/finstagram_posts' do
+  photo_url = params[:photo_url]
+  @finstagram_post = FinstagramPost.new({ photo_url: photo_url, user_id: current_user.id })
+  if @finstagram_post.save
+    redirect(to('/'))
+  else
+    erb(:"finstagram_posts/new")
+  end
+end
+before '/finstagram_posts/new' do
+  redirect to('/login') unless logged_in?
+end
+
+
+# form to create new post
+get '/finstagram_posts/new' do
+  @finstagram_post = FinstagramPost.new
+  erb(:"finstagram_posts/new")
+end
+
+
+# show a single post
+get '/finstagram_posts/:id' do
+  @finstagram_post = FinstagramPost.find(params[:id])
+  erb(:"finstagram_posts/show")
+end
+
+post '/comments' do
+    # point values from params to variables
+  text = params[:text]
+  finstagram_post_id = params[:finstagram_post_id]
+
+  # instantiate a comment with those values & assign the comment to the `current_user`
+  comment = Comment.new({ text: text, finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+
+  # save the comment
+  comment.save
+
+  # `redirect` back to wherever we came from
+  redirect(back)
+end
+
+post '/likes' do
+    
+  finstagram_post_id = params[:finstagram_post_id]
+
+
+  like = Like.new({ finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+
+
+  # save the like
+  like.save
+
+  # `redirect` back to wherever we came from
+  redirect(back)
+end
+
+delete '/likes/:id' do
+  like = Like.find(params[:id])
+  like.destroy
+  redirect(back)
+end
